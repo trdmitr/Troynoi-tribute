72dd9ad3e8130d2e2e102c7c4804cfbc5b448e1e
trdmitr <trdmitr@gmail.com>
2022-10-18 21:23:48 +0400

    useQuery

--- src/components/Cavers/Cavers.jsx
+++ src/components/Cavers/Cavers.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState, useQuery } from 'react'
 import classes from './Cavers.module.css'
 import { useNavigate } from 'react-router-dom'
 import cl from '../Pub.module.css'
@@ -17,7 +17,7 @@ const Cavers = () => {
 
 
     }
-    useEffect(() => {
+    useQuery(() => {
         getCavers()
 
     }, [])
