5d1a244e2fd75526dcf41ee76220bc94ee396df8
trdmitr <trdmitr@gmail.com>
2022-10-24 16:38:12 +0400

    add api file

--- src/components/API/SingleServuce.jsx
+++ src/components/API/SingleServuce.jsx
@@ -14,12 +14,24 @@ export const SingleServuce = () => {
     const apiUrl = 'https://api.jsonbin.io/v3/b/634d91750e6a79321e2bb874';
     axios.get(apiUrl, config)
     .then((response) => {
-      const allCavers = response.data;
-      setSingl(allCavers);
-    });
+    //   const allCavers = response.data;
+    setIsLoaded(true);
+      setSingl(response.data);
+    }).catch(error => {
+        setIsLoaded(true);
+        setError(error);
+      });
   }, [setSingl]);
-  if (!singl) return "Нет данных!";
-  return (
+//   if (error) return `Error: ${error.message}`;
+//   if (!singl) return "Нет данных!";
+if (error) {
+    return <div>Ошибка: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Загрузка...</div>;
+  } else {
+    return (
     <div>SingleServuce</div>
   )
+  }
+  
 }
